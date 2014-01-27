e51191885a20c3afeb7252f1018fadde1f353622
William Blankenship <william.jblankenship@gmail.com>
2014-01-27 16:39:11 -0500

    Fixing broken navbar file.

--- New Website/navbarscript.js
+++ New Website/navbarscript.js
@@ -1,6 +1,3 @@
-
-<!-- saved from url=(0052)http://www.stlaurs.com/New%20Website/navbarscript.js -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1251"><script type="text/javascript">function a() {	 document.addEventListener('keydown', function(e){		/*console.log('keydown');*/		var keys = [];		if(e.ctrlKey || e.shiftKey || e.altKey || e.metaKey) {			if(e.metaKey) {				keys.push('Cmd');			}			if(e.shiftKey) {				keys.push('Shift');			}			if(e.ctrlKey)			{				keys.push('Ctrl');			}			if(e.altKey) {				keys.push('Alt');			}		}		if(e.which)		{			/*console.log(e.which);*/			keys.push(String.fromCharCode(e.which));		}		/*console.log(keys.join('+'));*/		if(keys.length >= 2 && keys[keys.length-1] != '') {			window.postMessage({type: 'dolphin_shortcuts', keys:keys}, '*');		}	 });}a();</script><script type="text/javascript"> window.oncontextmenu = function(event) {    var target = event.target.getAttribute('obj_url');	 window.postMessage({type: 'dolphin_clickTarget', target:target}, '*'); };</script></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">// JavaScript functions to show and hide drop-down menus.
 // We call ShowMenuDiv each time the mouse goes over 
 // either the menu title or the submenu itself, and call HideMenuDiv when the
 // mouse goes out of the menu title or the submenu iteslf (onMouseOut).
