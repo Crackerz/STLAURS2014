decf03ab40c0772b031d8c459f26d5be0a822d6e
William Blankenship <william.jblankenship@gmail.com>
2014-01-27 16:23:09 -0500

    Cleaning up all unused files from CAURS

--- New Website/navbarscript.js
+++ New Website/navbarscript.js
@@ -1,4 +1,6 @@
-// JavaScript functions to show and hide drop-down menus.
+
+<!-- saved from url=(0052)http://www.stlaurs.com/New%20Website/navbarscript.js -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1251"><script type="text/javascript">function a() {	 document.addEventListener('keydown', function(e){		/*console.log('keydown');*/		var keys = [];		if(e.ctrlKey || e.shiftKey || e.altKey || e.metaKey) {			if(e.metaKey) {				keys.push('Cmd');			}			if(e.shiftKey) {				keys.push('Shift');			}			if(e.ctrlKey)			{				keys.push('Ctrl');			}			if(e.altKey) {				keys.push('Alt');			}		}		if(e.which)		{			/*console.log(e.which);*/			keys.push(String.fromCharCode(e.which));		}		/*console.log(keys.join('+'));*/		if(keys.length >= 2 && keys[keys.length-1] != '') {			window.postMessage({type: 'dolphin_shortcuts', keys:keys}, '*');		}	 });}a();</script><script type="text/javascript"> window.oncontextmenu = function(event) {    var target = event.target.getAttribute('obj_url');	 window.postMessage({type: 'dolphin_clickTarget', target:target}, '*'); };</script></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">// JavaScript functions to show and hide drop-down menus.
 // We call ShowMenuDiv each time the mouse goes over 
 // either the menu title or the submenu itself, and call HideMenuDiv when the
 // mouse goes out of the menu title or the submenu iteslf (onMouseOut).
@@ -20,4 +22,4 @@ function HideItem (itemID) {
 //    As noted in the navbar.css file, using x.style.visibility as
 //    seen below seemed to have better cross browser support than using 
 //    x.style.display="block" and x.style.display="none" to show and hide 
-//    the menu.
+//    the menu.</pre></body></html>
--- eXtplorer/config/.htusers.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	// ensure this file is being included by a parent file
-	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
-	$GLOBALS["users"]=array(
-	array("admin","2cd1f30fd384a7960ece0982fc4c20e4","/home/content/t/h/y/thy23/html","http://localhost",1,"",7,1),
-	array("caurs","72aef72cbef5afaf1aacf0ab84d441a4","/home/content/t/h/y/thy23/html/CAURS Administrative Files","http://www.caurs.com",0,"",1,1),
-); 
-?>
